c8d1e868bd56700993ef6ee544ad9a27f0cb4300
AxomLegend <128212279+AxomLegend@users.noreply.github.com>
2023-04-20 06:25:41 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='Unavailable'>Click here</a>\n○ Channel : @Anime_in_Hindi_Chat_Group\n○ Support Group : @Dub_Animes</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/Sub_Anime_in_Hindi?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/+KYSokEH6E701NGFl'>Unavailable</a>\n○ Channel : @Dub_Animes\n○ Support Group : @Anime_in_Hindi_Chat_Group</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
